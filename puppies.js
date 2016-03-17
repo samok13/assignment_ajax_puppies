a6419b73086e7e5e4854c9566deec1edc32289e3
Sam O'Keefe <samok13@gmail.com>
2016-03-17 15:42:26 -0700

    finished puppies

--- puppies.js
+++ puppies.js
@@ -26,7 +26,7 @@ Puppies = (function() {
       var created_at =  $.timeago(puppy.created_at)
       var id = puppy.id
 
-      var $newPuppy = $("<tr><td id='puppy-name'>" + name + "</td><td id='puppy-breed'>" + breed + "</td><td id='puppy-created'>created " + created_at + "</td><td id='puppy-adopt'><a href='#' data-puppy-id='"+ id +"'>Adopt</a></td></tr>");
+      var $newPuppy = $("<tr><td id='puppy-name'>" + name + "</td><td id='puppy-breed'>" + breed + "</td><td id='puppy-created'>created " + created_at + "</td><td id='puppy-adopt'><a class='puppy-adopt' href='#' data-puppy-id='"+ id +"'>Adopt</a></td></tr>");
 
     $('#puppy-list').append($newPuppy)
   };
@@ -56,7 +56,7 @@ Puppies = (function() {
 
   var registerPuppy = function(name, breed) {
     var data = JSON.stringify({name: name, breed_id: breed});
-    console.log(data);
+   
     $.ajax( {
       method: "POST",
       url: baseurl + "puppies.json",
@@ -66,16 +66,25 @@ Puppies = (function() {
         getPuppies();
       },
       error: function(){
-        alert('Could not registrer your Puppy');
+        alert('Could not register your Puppy');
       }
     })
   };
 
   var removePuppy = function(e){
-    var puppyId = $(e.target).data("puppy-id")
-    console.log(puppyId);
-     
-  }
+    var puppyId = $(e.target).data("puppy-id");
+
+    $.ajax({
+      method:"DELETE",
+      url:"https://ajax-puppies.herokuapp.com/puppies/"+ puppyId +".json",
+      success: function(){
+        $(e.target).closest('tr').fadeOut();
+      },
+      error: function(xhr, status, error){
+        console.error(error);
+      }
+    })
+  };
 
   return {
     getPuppies: getPuppies,
@@ -101,9 +110,26 @@ $(function() {
       e.preventDefault();
   })
 
-  $('body').on('click', '.puppy-adopt', function(e){
-    console.log('adopt');
+  $('#puppy-list').on("click", ".puppy-adopt", function(e){
     Puppies.removePuppy(e);
     e.preventDefault();
   })
+
+  $(document).ajaxStart(function(){
+    $('.status').stop().fadeIn();
+    $('.status').delay(1000).html("<div class='waiting'>Sorry this is taking so long.</div>");
+    $('.status').html("<div class='waiting'>Waiting</div>");
+  })
+
+  $(document).ajaxSuccess(function(){ 
+    $('.status').html("<div class='finished'>Finished!</div>");
+    $('.status').stop().fadeIn();
+    $('.status').delay(1000).fadeOut();
+  })
+
+  $(document).on("ajaxError",function(){
+    $('.status').stop().fadeIn();
+    $('.status').html("<div class='error'>Puppy Name is required to be present.</div>");
+  })
+
 });
